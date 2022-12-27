af0ed335a47ac26f7d8eef9dd58036cfaecf51c5
Vladimir_Leonov <2227331.spb@gmail.com>
2022-12-27 18:10:20 +0700

    final version 2

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,11 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App.jsx';
-global.ResizeObserver = require('resize-observer-polyfill')
-
+global.ResizeObserver = require('resize-observer-polyfill');
 
 describe('Components test', () => {
-    test('renders learn react link', () => {
-        render(<App />);
-       screen.debug()
-      });
+  test('renders learn react link', () => {
+    render(<App />);
+    screen.debug();
+  });
 });
--- frontend/src/components/version_1/OverviewFlow.jsx
+++ frontend/src/components/version_1/OverviewFlow.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer, useState } from 'react';
+import React, { useEffect, useReducer} from 'react';
 import ReactFlow from 'react-flow-renderer/nocss';
 import 'react-flow-renderer/dist/style.css';
 import 'react-flow-renderer/dist/theme-default.css';
@@ -8,27 +8,22 @@ import reducer, { loadBlocks, initialState, addBlock, addEdg } from './Reducer';
 const OverviewFlow = () => {
   const [blocks, dispatch] = useReducer(reducer, initialState);
 
-
   useEffect(() => {
     dispatch(loadBlocks());
   }, [blocks]);
 
-
-
   const addBlockFunc = (event) => {
-    const newBlockId = Math.random()
+    const newBlockId = Math.random();
 
     dispatch(
       addBlock({
         id: event.target.getAttribute('data-id'),
         position: event.target.getAttribute('style'),
         randomCoordinate: Math.floor(Math.random() * (500 - 80) + 80),
-        newBlockId
+        newBlockId,
       })
     );
   };
-
-  
 
   return (
     <div className="cantainer">
