3e78a72a34c2236d31408a200fa9f90c58bdd2a5
Vladimir_Leonov <2227331.spb@gmail.com>
2022-12-27 17:51:02 +0700

    version 2.1 test debug ok

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,13 +1,11 @@
-import { create } from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
 import App from './App.jsx';
+global.ResizeObserver = require('resize-observer-polyfill')
 
 
 describe('Components test', () => {
-test ('App test', () => {
-const component = create(<App/>)
-const root = component.root
-let a = root.findAllByType('a')
-expect(a.length).toBe(1)
-
-});
+    test('renders learn react link', () => {
+        render(<App />);
+       screen.debug()
+      });
 });
